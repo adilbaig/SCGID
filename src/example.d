2880c490a9fca214b55e3fdfa4c010b2038afafa
Adil <adil@stockopedia.co.uk>
2012-03-18 20:10:54 +0530

    Documentation

--- src/example.d
+++ src/example.d
@@ -10,7 +10,18 @@ int main(string args[])
     
     auto port = to!ushort(args[1]);
     
+    /**
+       The following call will start the SCGI server on port "port" and go into and infinite loop.
+    */
     SCGIServer(port, function void(const Request request, Socket connection){
+        /*
+           This function is called once per request. The Request struct contains the headers
+           as well GET and POST variables (parsed and urldecoded). The Socket is the connection 
+           to the client. Use connection.send("string") to send responses back to the client.
+           You can close the connection, or the library will do it for you once the function completes.   
+        */
+        
+        connection.send("Status: 200 OK\r\n\r\n");
         connection.send("Content-Type: text/html\r\n\r\n");
 
         connection.send("<h1>Headers</h1><ul>");
