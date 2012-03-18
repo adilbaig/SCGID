9cfdf59c6f1679dc4eef6605436279dd396661bf
Adil <adil@stockopedia.co.uk>
2012-03-18 19:25:50 +0530

    Added urldecode

--- src/scgi.d
+++ src/scgi.d
@@ -2,8 +2,8 @@ module scgi;
 
 private :
     import std.conv : to; 
-    import std.string : indexOf;
-    import std.string : split;  
+    import std.string : indexOf, split;
+    import std.regex;
     import std.stdio;
     
 public import std.socket;
@@ -28,10 +28,53 @@ private :
         foreach(s; arr)
         {
             auto i = indexOf(s,'=');
-            ret[s[0 .. i]] = s[i + 1 .. $];
+            auto k = urldecode(s[0 .. i]);
+            auto v = urldecode(s[i + 1 .. $]);
+            ret[k] = v;
         }
         
         return ret;
+    }
+
+    string urldecode(string url)
+    {
+        int l;
+    
+        int hexvalue(char ch)
+        {
+            if (ch >= '0' && ch <= '9')
+                return ch - '0';
+            if (ch >= 'a' && ch <= 'f')
+                return ch - 'a' + 10;
+            if (ch >= 'A' && ch <= 'F')
+                return ch - 'A' + 10;
+            throw new Error("URL has an encoding value that is outside of the hexadecimal range.");
+        }
+    
+        for (int c; c < url.length; c++, l++)
+            if (url[c] == '%')
+            {
+                if (c + 3 > url.length)
+                    throw new Error("URL has an encoding marker ('%') that is not followed by two characters.");
+                c += 2;
+            }
+    
+        if (l == url.length)
+            return url;
+    
+        auto result = new char[l];
+        l = 0;
+    
+        for (int c; c < url.length; c++, l++)
+            if (url[c] == '%')
+            {
+                result[l] = to!char(hexvalue(url[c + 1]) * 16 + hexvalue(url[c + 2]));
+                c += 2;
+            }
+            else
+                result[l] = url[c];
+    
+        return cast(immutable)result;
     }
 
     struct Request{
@@ -107,7 +150,7 @@ public :
             if (body_length > 0)
                 body_data = receive[s + header_length + 1 .. body_length];
             
-            debug{ writeln("BUFFER2 : ", receive); }
+            debug{ writeln("RECEIVED DATA : ", receive); }
             
             Request r = {headers, null, null, cast(immutable)body_data};
             
