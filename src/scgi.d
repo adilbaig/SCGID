c18c6a6c60e7cf6cf0c66c1d0eade3f56bb58bc5
Adil <adil@stockopedia.co.uk>
2012-07-08 23:03:35 +0530

    Non blocking listener with select

--- src/scgi.d
+++ src/scgi.d
@@ -3,12 +3,108 @@ module scgi;
 private :
     import std.conv : to; 
     import std.string : indexOf, split;
-    //import std.regex;
     import std.stdio;
     
 public import std.socket;
     
+public :
+    
+    void SCGIServer(ushort port, void function(const Request, Socket) handler)
+    {
+        Socket listener = new TcpSocket();
+        scope(exit) listener.close();
+        assert(listener.isAlive);
+        listener.blocking = false;
+        listener.bind(new InternetAddress(port));
+        listener.listen(10);
+        writefln("Listening on port %d.", port);
+        
+        uint maxConnections = 60;
+        SocketSet sset = new SocketSet(maxConnections + 1);
+        
+        while(true)
+        {
+            sset.reset();
+            sset.add(listener);
+            
+            Socket.select(sset, null, null);
+            
+            if(sset.isSet(listener))
+            {
+                Socket conn = listener.accept();
+                writef("Received connection from %s .. ", conn.remoteAddress().toString());
+                
+                try{
+                    Request r = getRequest(conn);
+                    handler(r, conn);
+                }catch(Exception e)
+                {
+                    writeln(e);
+                }
+                finally
+                {
+                    if (conn.isAlive)
+                    {
+                        conn.close();
+                        writeln("Connection closed");
+                    }
+                }
+            }
+        }
+    }
+
 private :
+    
+    Request getRequest(Socket conn)
+    {
+        char[1024 * 2] buff;
+        char[] receive;
+        ulong bytes_read, header_length;
+        long s;
+        
+        do {
+            bytes_read += conn.receive(buff);
+            receive ~= buff;
+            
+            //If the header length has not been calculated, do it.
+            if(!header_length)
+            {
+                s = indexOf(receive,':');
+                if (s == -1)
+                    throw new Exception("Invalid SCGI request! - Length not given");
+                   
+                header_length = to!ulong(receive[0 .. s]);
+            }
+            
+        }while(bytes_read > 0 
+            && bytes_read < header_length);
+        
+        if (Socket.ERROR == bytes_read)
+            throw new Exception("Connection error.");
+        else if (0 == receive.length)
+            throw new Exception("Connection closed.");
+        
+        auto headers = getHeaders(cast(immutable)receive[s + 1 .. s + header_length]);
+        if ("CONTENT_LENGTH" !in headers)
+            throw new Exception("Invalid SCGI request! - Header CONTENT_LENGTH not found");
+        
+        auto body_length = to!uint(headers["CONTENT_LENGTH"]);
+        
+        char[] body_data;
+        if (body_length > 0)
+            body_data = receive[s + header_length + 1 .. body_length];
+        
+        debug{ writeln("RECEIVED DATA : ", receive); }
+        
+        Request r = {headers, null, null, cast(immutable)body_data};
+        if (headers["REQUEST_METHOD"] == "GET")
+            r.get =  getParams(cast(immutable)headers["QUERY_STRING"]);
+        if(headers["REQUEST_METHOD"] == "POST")
+            r.post = getParams(cast(immutable)body_data[1 .. $]);
+        
+        return r;
+    }
+
     string[string] getHeaders(string buffer)
     {
         string[] arr = split(buffer,"\0");
@@ -84,89 +180,3 @@ private :
         string content;
     }
 
-
-public :
-    
-    void SCGIServer(ushort port, void function(const Request, Socket) handler)
-    {
-        Socket listener = new TcpSocket();
-        scope(exit) listener.close();
-        assert(listener.isAlive);
-        listener.bind(new InternetAddress(port));
-        listener.listen(10);
-        writefln("Listening on port %d.", port);
-        
-        while(true)
-        {
-            Socket conn = listener.accept();
-            writef("Receieved connection from %s .. ", conn.remoteAddress().toString());
-            
-            char[1024 * 2] buff;
-            char[] receive;
-            ulong bytes_read, header_length;
-            long s;
-            
-            do {
-                bytes_read += conn.receive(buff);
-                receive ~= buff;
-                
-                debug{ writeln("BYTES_READ" , bytes_read); }
-                
-                //If the header length has not been calculated, do it.
-                if(!header_length)
-                {
-                    s = indexOf(receive,':');
-                    if (s == -1)
-                    {
-                        writeln("Invalid SCGI request! - Length not given");
-                        goto END;
-                    }
-                       
-                    header_length = to!ulong(receive[0 .. s]);
-                }
-                
-            }while(bytes_read > 0 
-                && bytes_read < header_length);
-            
-            if (Socket.ERROR == bytes_read)
-                writeln("Connection error.");
-            else if (0 == bytes_read)
-                writeln("Connection closed.");
-                    
-            if(!receive.length)
-                continue;
-            write(to!string(bytes_read) ~ " bytes read .. ");
-            
-            auto headers = getHeaders(cast(immutable)receive[s + 1 .. s + header_length]);
-            if ("CONTENT_LENGTH" !in headers)
-            {
-                writeln("Invalid SCGI request! - Header CONTENT_LENGTH not found");
-                goto END;
-            }   
-            
-            auto body_length = to!uint(headers["CONTENT_LENGTH"]);
-            
-            char[] body_data;
-            if (body_length > 0)
-                body_data = receive[s + header_length + 1 .. body_length];
-            
-            debug{ writeln("RECEIVED DATA : ", receive); }
-            
-            Request r = {headers, null, null, cast(immutable)body_data};
-            
-            if (headers["REQUEST_METHOD"] == "GET")
-                r.get =  getParams(cast(immutable)headers["QUERY_STRING"]);
-            if(headers["REQUEST_METHOD"] == "POST")
-                r.post = getParams(cast(immutable)body_data[1 .. $]);
-                
-            handler(r, conn);
-            
-//Label only used when an invalid SCGI request is made
-END:        
-            if (conn.isAlive)
-            {
-                writeln("Connection closed");
-                conn.close();
-            }
-        }
-    }
